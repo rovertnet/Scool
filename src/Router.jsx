6347f4f4d7ed12b93a4e4fcb02a470bfa33ad210
Robert Matundu <matundukabamba@gmail.com>
2024-04-04 20:50:36 +0200

    last commit

---
+++ src/Router.jsx
@@ -0,0 +1,40 @@
+
+//les routes de toutes les pages du site
+
+import { createBrowserRouter } from "react-router-dom";
+
+const router =  createBrowserRouter([
+
+  {
+    path: "/",
+    element:
+  },
+
+  {
+    path: "/solution",
+    element:
+  },
+
+  {
+    path: "/about",
+    element:
+  },
+
+  {
+    path: "/faq",
+    element:
+  },
+
+  {
+    path: "/blog",
+    element:
+  },
+
+  {
+    path: "/contact",
+    element:
+  },
+
+]);
+
+export default router;
