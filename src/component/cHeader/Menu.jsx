6396e32d2884287d101c4d1735e6d9df47838e3e
Robert Matundu <matundukabamba@gmail.com>
2024-04-05 01:07:45 +0200

    last commit

--- src/component/cHeader/Menu.jsx
+++ src/component/cHeader/Menu.jsx
@@ -1,5 +1,7 @@
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
+import { FaPhoneVolume } from "react-icons/fa6";
+import { MdOutlineWatchLater } from "react-icons/md";
+import { IoLocationOutline } from "react-icons/io5";
 
 const Menu = () => {
   return (
@@ -14,16 +16,38 @@ const Menu = () => {
             {/* */}
             <div className="flex justify-center items-center">
               <div className="flex gap-5">
-                
+                <FaPhoneVolume className=" font-extrabold text-3xl text-green-600" />
+                <div>
+                  <h3 className=" text-black text-2xl font-bold">Appel</h3>
+                  <p className=" text-green-600 text-xl font-semibold">
+                    (243) 998724266{" "}
+                  </p>
+                </div>
               </div>
-              <div className="flex gap-5"></div>
-              <div className="flex gap-5"></div>
+              <div className="flex gap-5">
+                <MdOutlineWatchLater />
+                <div>
+                  <h3 className=" text-black text-2xl font-bold">Horaire</h3>
+                  <p className=" text-green-600 text-xl font-semibold">
+                    (243) 998724266{" "}
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-5">
+                <IoLocationOutline />
+                <div>
+                  <h3 className=" text-black text-2xl font-bold">Location</h3>
+                  <p className=" text-green-600 text-xl font-semibold">
+                    (243) 998724266{" "}
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </>
   );
-}
+};
 
-export default Menu
+export default Menu;
