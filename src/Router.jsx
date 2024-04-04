968f4086513a99efe87878ac999c5d6a92bfed67
Robert Matundu <matundukabamba@gmail.com>
2024-04-04 21:08:12 +0200

    last commit

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,39 +2,43 @@
 //les routes de toutes les pages du site
 
 import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Solution from "./pages/Solution";
+import About from "./pages/About";
+import Faq from "./pages/Faq";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
 
-const router =  createBrowserRouter([
-
+const router = createBrowserRouter([
   {
     path: "/",
-    element:
+    element: <Home />,
   },
 
   {
     path: "/solution",
-    element:
+    element: <Solution />,
   },
 
   {
     path: "/about",
-    element:
+    element: <About />,
   },
 
   {
     path: "/faq",
-    element:
+    element: <Faq />,
   },
 
   {
     path: "/blog",
-    element:
+    element: <Blog />,
   },
 
   {
     path: "/contact",
-    element:
+    element: <Contact />,
   },
-
 ]);
 
 export default router;
