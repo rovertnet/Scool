7ba37ab611d554ea0f79865131c6424c5725ff26
Robert Matundu <matundukabamba@gmail.com>
2024-04-04 21:48:04 +0200

    last commit

--- src/component/cHeader/Menu.jsx
+++ src/component/cHeader/Menu.jsx
@@ -1,15 +1,29 @@
+import { Link } from "react-router-dom"
 
 
 const Menu = () => {
   return (
     <>
       <div className="min-h-full">
+        {/* Top Bar begin */}
         <div className=" bg-slate-50">
-
+          <div className="flex justify-between items-center">
+            <Link to={"/"}>
+              <img src="../../public/vite.svg" alt="logo" className="" />
+            </Link>
+            {/* */}
+            <div className="flex justify-center items-center">
+              <div className="flex gap-5">
+                
+              </div>
+              <div className="flex gap-5"></div>
+              <div className="flex gap-5"></div>
+            </div>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default Menu
