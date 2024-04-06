e8f5b17c2fbba3f7503d84a978e8bc9de4f0278a
Robert Matundu <matundukabamba@gmail.com>
2024-04-06 19:23:37 +0200

    another commit

--- src/component/cHeader/Menu.jsx
+++ src/component/cHeader/Menu.jsx
@@ -43,7 +43,7 @@ const Menu = () => {
                     </Link>
                     {/* */}
                     <div className=" hidden md:block">
-                      <div className="flex md:flex flex-col justify-center items-center gap-5">
+                      <div className="flex justify-center items-center gap-5">
                         <div className="flex gap-2">
                           <LiaPhoneVolumeSolid className=" font-extrabold text-6xl text-green-600" />
                           <div>
@@ -82,7 +82,7 @@ const Menu = () => {
 
                     <div className="-mr-2 flex md:hidden p-3">
                       {/* Mobile menu button */}
-                      <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-purple p-2 text-green-700 hover:bg-green-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-green-700">
+                      <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-purple p-2 text-green-700 hover:bg-green-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 ring-offset-2 focus:ring-offset-green-700 ring-offset-green-700">
                         <span className="absolute -inset-0.5" />
                         <span className="sr-only">Open main menu</span>
                         {open ? (
@@ -155,9 +155,9 @@ const Menu = () => {
                     ))}
                   </div>
                   <div className="border-t border-green-700 pt-4">
-                    <div className="flex md:flex flex-col space-x-2">
+                    <div className="flex md:flex flex-col gap-2 px-10">
                       <div className="flex gap-2">
-                        <LiaPhoneVolumeSolid className=" font-extrabold text-xl text-green-600" />
+                        <LiaPhoneVolumeSolid className=" font-extrabold text-2xl text-green-600" />
                         <div>
                           <h3 className=" text-black text-lg font-bold">
                             Appel
@@ -168,7 +168,7 @@ const Menu = () => {
                         </div>
                       </div>
                       <div className="flex gap-2">
-                        <MdOutlineWatchLater className=" font-extrabold text-xl text-green-600" />
+                        <MdOutlineWatchLater className=" font-extrabold text-2xl text-green-600" />
                         <div>
                           <h3 className=" text-black text-lg font-bold">
                             Horaire
@@ -179,7 +179,7 @@ const Menu = () => {
                         </div>
                       </div>
                       <div className="flex gap-2">
-                        <IoLocationOutline className=" font-extrabold text-xl text-green-600" />
+                        <IoLocationOutline className=" font-extrabold text-2xl text-green-600" />
                         <div>
                           <h3 className=" text-black text-lg font-bold">
                             Location
@@ -190,8 +190,8 @@ const Menu = () => {
                         </div>
                       </div>
                     </div>
-                    <div className="mt-3 space-y-1 px-2">
-                      <button className=" bg-black font-semibold rounded-full px-7 py-5 text-2xl text-slate-100">
+                    <div className="mt-3 space-y-1 px-10 pb-5">
+                      <button className=" bg-green-700 font-semibold rounded-md px-20 py-2 text-xl text-slate-100">
                         Se connecter
                       </button>
                     </div>
