d7a4fab283634662429a0c44d3d092dc3b3750d7
Robert Matundu <matundukabamba@gmail.com>
2024-04-07 22:59:29 +0200

    another commit

--- src/component/cAbout/Banner.jsx
+++ src/component/cAbout/Banner.jsx
@@ -2,15 +2,15 @@ const Banner = () => {
   return (
     <>
       <div>
-        <div className=" bg-center bg-cover p-40 w-full h-full bg-[url('../../public/medecin_banner.png')] my-5 md:my-52">
+        <div className=" bg-center bg-cover p-36 w-full h-full bg-[url('../../public/medecin_banner.png')] my-5 md:my-52">
           <div className=" flex flex-col justify-start items-start gap-10">
             <span className=" font-medium text-md text-green-500">
-              Le titre
+              M U S D E E L
             </span>
             <p className=" font-extrabold text-4xl text-slate-300">
-              Lorem ipsum dolor sit amet <br /> consectetur
+              Retrouvons la santé <br /> ensemble
             </p>
-            <div className="">
+            <div className=" pt-4">
               <button className=" bg-green-700 rounded-full md:px-4 px-9 md:py-3 py-5 text-lg font-bold text-white">
                 Adherer maintenant
               </button>
