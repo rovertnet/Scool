cb60cda18b1b1a824b30b20d365978a6055711eb
Robert Matundu <matundukabamba@gmail.com>
2024-04-08 21:14:07 +0200

    another commit

--- src/component/cHome/About.jsx
+++ src/component/cHome/About.jsx
@@ -4,15 +4,17 @@ import { MdOutlineSettingsInputComposite } from "react-icons/md";
 const About = () => {
   return (
     <>
-      <div className="mx-10">
+      <div className=" my-10 md:my-12 px-10 md:px-12">
         <div className="flex flex-row justify-center items-center">
-          <div className=" w-full md:w-6/4 h-full">
+          <div className=" w-full md:w-4/4 h-full">
             <img src="../../public/about.png" className="" alt="about image" />
           </div>
 
-          <div className=" w-full md:w-6/6">
-            <h2 className="">Bienvenue chez musdeel</h2>
-            <p className="">
+          <div className=" w-full md:w-4/4 md:mt-4">
+            <h2 className=" uppercase font-bold text-2xl text-green-700">
+              Bienvenue chez musdeel
+            </h2>
+            <p className=" text-xl text-slate-800 font-semibold text-justify md:my-6 my-4">
               Nous assurons la proximité avec un accompagnement à chaque étape
               des soins, jusqu’à la guérison du patient scolarisé et à jour dans
               ses cotisations
@@ -39,7 +41,7 @@ const About = () => {
                 Confiance
               </p>
             </div>
-            <p className="">
+            <p className=" font-medium text-xl text-justify text-slate-800 md:my-6">
               Nous accompagnons nos adhérents dès la 4è années de scolarité
               jusqu’à la fin du cycle universitaire et/ou les apprentis inscrits
               régulièrement dans une formation professionnelle. Notre marque
@@ -47,7 +49,7 @@ const About = () => {
               sommes universels dans notre collaboration avec les adhérents sans
               distinction de la province d’origine
             </p>
-            <p className="">
+            <p className=" font-medium text-xl text-justify text-slate-800">
               Nous assurons la qualité de service à nos adhérents et à nos
               partenaires afin de vendre une bonne image de notre structure. Ce
               sérieux se traduit par l’accompagnement rigoureux de nos adhérents
@@ -55,6 +57,11 @@ const About = () => {
               pour satisfaire nos sociétaires et nos partenaires. En résumé : la
               qualité de service
             </p>
+            <div className=" mt-5 md:mt-5">
+              <button className=" text-white rounded-full text-xl px-6 md:px-16 py-3 font-semibold bg-green-700">
+                En svoir plus
+              </button>
+            </div>
           </div>
         </div>
       </div>
