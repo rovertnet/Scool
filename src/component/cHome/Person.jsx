f977834722f52ff861b6c3cac54c8a8a359d47df
Robert Matundu <matundukabamba@gmail.com>
2024-04-09 18:10:28 +0200

    another commit

--- src/component/cHome/Person.jsx
+++ src/component/cHome/Person.jsx
@@ -2,9 +2,13 @@
 const Person = () => {
   return (
     <>
-      
+      <div className="">
+        <h2 className=" text-lg text-green-700 font-medium md:text-xl">D es soins auxquels vous pouvez croire</h2>
+        <h3 className=""></h3>
+        <div className=""></div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Person
