684c804e07062b5ef6219d14a41f4193341ce6be
Robert Matundu <matundukabamba@gmail.com>
2024-04-09 18:15:09 +0200

    another commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Menu from "../component/cHeader/Menu";
 import Banner from "../component/cAbout/Banner";
 import About from "../component/cHome/About";
+import Person from "../component/cHome/Person";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
 
       <Banner />
       <About />
+      <Person />
     </>
   );
 };
