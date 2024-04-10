414643a04262bac5f91ffef84b0230a9730cdb69
Robert Matundu <matundukabamba@gmail.com>
2024-04-10 14:30:06 +0200

    another commit

--- src/component/cHome/Person.jsx
+++ src/component/cHome/Person.jsx
@@ -5,13 +5,15 @@ const Person = () => {
       <div className=" my-10 md:my-14 px-10 md:px-12 lg:my-14 lg:px-12 block">
         <div className="text-center">
           <h2 className=" text-lg text-green-700 font-medium md:text-xl">
-            D es soins auxquels vous pouvez croire
+            Des soins auxquels vous pouvez croire
           </h2>
           <h3 className=" text-black text-2xl uppercase font-bold">
             Notre personnalité
           </h3>
         </div>
-        <div className=""></div>
+        <div className=" flex justify-center items-center gap-5">
+          
+        </div>
       </div>
     </>
   );
