aa08d7fd526df89a4e229e371db80ce450a9ba11
Robert Matundu <matundukabamba@gmail.com>
2024-04-11 23:06:27 +0200

    another commit

--- src/component/cHome/Question.jsx
+++ src/component/cHome/Question.jsx
@@ -3,7 +3,7 @@ import { VscWorkspaceTrusted } from "react-icons/vsc";
 const Question = () => {
   return (
     <>
-      <div className="mx-10 md:mx-10 ùd">
+      <div className="mx-10 md:mx-10 md:my-10 my5">
         <div className="text-center md:py-10 py-10">
           <h1 className=" text-xl md:text-2xl text-center font-extrabold uppercase">
             Questions fréquemment posées
@@ -15,21 +15,21 @@ const Question = () => {
           </p>
         </div>
         <div className=" grid grid-cols-1 gap-10">
-          <div className="border-[1px] border-slate-400 rounded-lg bg-slate-100 ">
+          <div className="border-[1px] border-slate-400 rounded-lg bg-slate-50 ">
             <h2 className="text-xl md:text-lg font-bold text-black flex justify-between md:px-10 px-10 md:py-5 py-5">
               01. Quelles sont nos offres ?
               <VscWorkspaceTrusted className=" text-2xl md:text-xl text-green-500" />
             </h2>
           </div>
 
-          <div className="border-[1px] border-slate-400 rounded-lg bg-slate-100 ">
+          <div className="border-[1px] border-slate-400 rounded-lg bg-slate-50 ">
             <h2 className="text-xl md:text-lg font-bold text-black flex justify-between md:px-10 px-10 md:py-5 py-5">
               01. Quelles sont nos offres ?
               <VscWorkspaceTrusted className=" text-2xl md:text-xl text-green-500" />
             </h2>
           </div>
 
-          <div className="border-[1px] border-slate-400 rounded-lg bg-slate-100 ">
+          <div className="border-[1px] border-slate-400 rounded-lg bg-slate-50 ">
             <h2 className="text-xl md:text-lg font-bold text-black flex justify-between md:px-10 px-10 md:py-5 py-5">
               01. Quelles sont nos offres ?
               <VscWorkspaceTrusted className=" text-2xl md:text-xl text-green-500" />
