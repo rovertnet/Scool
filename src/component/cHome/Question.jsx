fbbbf7bf1fcb1350c96a8f18693e430745432896
Robert Matundu <matundukabamba@gmail.com>
2024-04-11 22:09:13 +0200

    another commit

--- src/component/cHome/Question.jsx
+++ src/component/cHome/Question.jsx
@@ -1,8 +1,8 @@
-
 const Question = () => {
   return (
     <>
       <div className="mx-10 md:mx-10">
+        <h1 className=" text-xl md:text-2xl">Questions fréquemment posées</h1>
         <div className=" grid grid-cols-1 gap-10">
           <div className="border-[1px] border-slate-400 rounded-lg bg-slate-100 ">
             <h2 className="text-xl md:text-lg font-bold text-black"></h2>
@@ -10,7 +10,7 @@ const Question = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Question
+export default Question;
