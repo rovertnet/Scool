752fc47ea6ff3a46c866f095c98b52ece0a70c36
Robert Matundu <matundukabamba@gmail.com>
2024-04-11 22:27:26 +0200

    another commit

--- src/component/cHome/Question.jsx
+++ src/component/cHome/Question.jsx
@@ -2,14 +2,14 @@ const Question = () => {
   return (
     <>
       <div className="mx-10 md:mx-10">
-        <div className="text-center">
-          <h1 className=" text-xl md:text-2xl text-center">
+        <div className="text-center md:py-10 py-10">
+          <h1 className=" text-xl md:text-2xl text-center font-extrabold">
             Questions fréquemment posées
           </h1>
-          <p className="">
-            Nous avons essayé de répondre aux questions les plus courantes. Si
-            vous en avez d autres, n hésitez pas à contacter notre équipe
-            amicale
+          <p className=" md:pt-3 pt-3">
+            Nous avons essayé de répondre aux questions les plus courantes. <br /> Si
+            vous en avez d autres, n hésitez pas à contacter notre <br /> équipe
+            amicale.
           </p>
         </div>
         <div className=" grid grid-cols-1 gap-10">
