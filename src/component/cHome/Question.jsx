6a652036813cd2881da0f5b3ee111c7b46b85365
Robert Matundu <matundukabamba@gmail.com>
2024-04-11 22:52:17 +0200

    another commit

--- src/component/cHome/Question.jsx
+++ src/component/cHome/Question.jsx
@@ -7,14 +7,16 @@ const Question = () => {
             Questions fréquemment posées
           </h1>
           <p className=" md:pt-3 pt-3">
-            Nous avons essayé de répondre aux questions les plus courantes. <br /> Si
-            vous en avez d autres, n hésitez pas à contacter notre <br /> équipe
-            amicale.
+            Nous avons essayé de répondre aux questions les plus courantes.{" "}
+            <br /> Si vous en avez d autres, n hésitez pas à contacter notre{" "}
+            <br /> équipe amicale.
           </p>
         </div>
         <div className=" grid grid-cols-1 gap-10">
           <div className="border-[1px] border-slate-400 rounded-lg bg-slate-100 ">
-            <h2 className="text-xl md:text-lg font-bold text-black"></h2>
+            <h2 className="text-xl md:text-lg font-bold text-black flex justify-between">
+              01. Quelles sont nos offres ?
+            </h2>
           </div>
         </div>
       </div>
