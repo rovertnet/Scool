7ea130fe19424adbea4d33c1e1fe456c5b59e69a
Robert Matundu <matundukabamba@gmail.com>
2024-04-11 17:42:18 +0200

    another commit

--- src/component/cAbout/Banner.jsx
+++ src/component/cAbout/Banner.jsx
@@ -2,11 +2,8 @@ const Banner = () => {
   return (
     <>
       <div>
-        <div className=" block md:bg-cover md:p-36 bg-cover p-36 w-full h-full bg-[url('../../public/image/fond.jpg')] mt-5 md:mt-52">
-          <div className=" flex flex-col md:flex-row-reverse justify-start items-start gap-10">
-            <div className="">
-              <img src="../../public/m1.png" alt="" className="md:h-full w-full" />
-            </div>
+        <div className=" md:bg-cover bg-cover bg-center md:p-36 p-36 w-full h-full bg-[url('../../public/123.jpg')] mt-5 md:mt-52">
+          <div className=" flex flex-col justify-start items-start gap-10">
             <div>
               <span className=" font-medium text-md text-green-500">
                 M U S D E E L
@@ -14,8 +11,8 @@ const Banner = () => {
               <p className=" font-extrabold text-4xl text-slate-300">
                 Retrouvons la santé <br /> ensemble
               </p>
-              <div className=" pt-4">
-                <button className=" bg-green-700 rounded-full md:px-9 px-9 md:py-3 py-5 text-lg font-bold text-white">
+              <div className="md:pt-3 pt-4">
+                <button className=" bg-green-700 rounded-full md:px-9 px-24 md:py-3 py-3 md:text-xl text-base font-bold text-white">
                   Adherer maintenant
                 </button>
               </div>
