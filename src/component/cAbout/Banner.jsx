8188b53e676f38da8ac172e095793244c47d1363
Robert Matundu <matundukabamba@gmail.com>
2024-04-11 16:55:25 +0200

    another commit

--- src/component/cAbout/Banner.jsx
+++ src/component/cAbout/Banner.jsx
@@ -3,7 +3,10 @@ const Banner = () => {
     <>
       <div>
         <div className=" block md:bg-cover md:p-36 bg-cover p-36 w-full h-full bg-[url('../../public/image/fond.jpg')] mt-5 md:mt-52">
-          <div className=" flex flex-col justify-start items-start gap-10">
+          <div className=" flex flex-col md:flex-row-reverse justify-start items-start gap-10">
+            <div className="">
+              <img src="../../public/m1.png" alt="" className="md:h-full w-full" />
+            </div>
             <div>
               <span className=" font-medium text-md text-green-500">
                 M U S D E E L
@@ -16,9 +19,6 @@ const Banner = () => {
                   Adherer maintenant
                 </button>
               </div>
-            </div>
-            <div className="">
-              
             </div>
           </div>
         </div>
