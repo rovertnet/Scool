4ce6e497956e04c880aac0aaeb78a3edab15f58c
Robert Matundu <matundukabamba@gmail.com>
2024-04-12 17:46:10 +0200

    another commit

--- src/component/cHome/Question.jsx
+++ src/component/cHome/Question.jsx
@@ -3,14 +3,12 @@ import { FaPlus } from "react-icons/fa6";
 import { useState } from "react";
 import { BiMinus } from "react-icons/bi";
 
-
-
 const Question = () => {
-  const [ viewQuestionOne, setViewQuestionOne ] = useState(false);
+  const [viewQuestionOne, setViewQuestionOne] = useState(false);
 
   const viewQuestOne = () => {
     setViewQuestionOne(!viewQuestionOne);
-  }
+  };
 
   return (
     <>
@@ -26,20 +24,56 @@ const Question = () => {
           </p>
         </div>
         <div className=" grid grid-cols-1 gap-10">
-          <div className="border-[1px] border-slate-400 rounded-lg bg-slate-50 ">
-            <h2 className="text-lg md:text-xl font-bold text-black flex justify-between md:px-10 px-10 md:py-5 py-5">
+          <div className="border-[1px] border-slate-300 rounded-lg bg-slate-50 shadow-md">
+            <div className="text-lg md:text-xl font-bold text-black flex justify-between md:px-10 px-10 md:py-5 py-5">
               01. Quelles sont nos offres ?
-              <FaPlus className=" text-2xl md:text-2xl text-green-500" />
-            </h2>
-            <div className="hidden md:hidden"></div>
+              <button
+                className=" cursor-pointer hover:bg-slate-400 hover:p-2 hover:md:p-2 hover:rounded-full"
+                onClick={viewQuestOne}
+              >
+                {viewQuestionOne ? (
+                  <BiMinus className=" text-2xl md:text-2xl font-bold text-black" />
+                ) : (
+                  <FaPlus className=" text-2xl md:text-2xl font-bold text-black" />
+                )}
+              </button>
+            </div>
+            <div
+              className={`${
+                viewQuestionOne
+                  ? "text-justify md:px-10 px-10 md:pb-3 pb-5"
+                  : "hidden md:hidden "
+              }`}
+            >
+              The main feature of Whitespace UI that it saves you time. If it
+              saves you time, it automatically saves you money. You no longer
+              have to create a new UI kit from scratch before starting each
+              project. Use Whitespace UI on an unlimited number of projects and
+              save hundreds of hours and thousands of dollars.
+            </div>
           </div>
 
-          <div className="border-[1px] border-slate-400 rounded-lg bg-slate-50 ">
-            <h2 className="text-lg md:text-xl font-bold text-black flex justify-between md:px-10 px-10 md:py-5 py-5">
+          <div className="border-[1px] border-slate-300 rounded-lg bg-slate-50 shadow-md">
+            <div className="text-lg md:text-xl font-bold text-black flex justify-between md:px-10 px-10 md:py-5 py-5">
               02. Comment devenir partenaire de la MUSDEEL ?
-              <VscWorkspaceTrusted className=" text-2xl md:text-xl text-green-500" />
-            </h2>
-            <div className="hidden md:hidden" >
+              <button
+                className=" cursor-pointer hover:bg-slate-400 hover:p-2 hover:md:p-2 hover:rounded-full"
+                onClick={viewQuestOne}
+              >
+                {viewQuestionOne ? (
+                  <BiMinus className=" text-2xl md:text-2xl font-bold text-black" />
+                ) : (
+                  <FaPlus className=" text-2xl md:text-2xl font-bold text-black" />
+                )}
+              </button>
+            </div>
+            <div
+              className={`${
+                viewQuestionOne
+                  ? "text-justify md:px-10 px-10 md:pb-3 pb-5"
+                  : "hidden md:hidden "
+              }`}
+            >
               The main feature of Whitespace UI that it saves you time. If it
               saves you time, it automatically saves you money. You no longer
               have to create a new UI kit from scratch before starting each
@@ -51,13 +85,6 @@ const Question = () => {
           <div className="border-[1px] border-slate-400 rounded-lg bg-slate-50 ">
             <div className="text-lg md:text-xl font-bold text-black flex justify-between md:px-10 px-10 md:py-5 py-5">
               03. Quelle est la politique de remboursement ?
-              <button className=" cursor-pointer" onClick={viewQuestOne}>
-                {viewQuestionOne ? (
-                  <BiMinus className=" text-2xl md:text-xl text-green-500" />
-                ) : (
-                  <VscWorkspaceTrusted className=" text-2xl md:text-xl text-green-500" />
-                )}
-              </button>
             </div>
             <div className=" hidden md:hidden"></div>
           </div>
