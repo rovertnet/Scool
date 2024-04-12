b470b5e6effe09e490558e9aa83418a41cdfb838
Robert Matundu <matundukabamba@gmail.com>
2024-04-12 16:17:04 +0200

    another commit

--- src/component/cHome/Question.jsx
+++ src/component/cHome/Question.jsx
@@ -1,6 +1,17 @@
 import { VscWorkspaceTrusted } from "react-icons/vsc";
+import { FaPlus } from "react-icons/fa6";
+import { useState } from "react";
+import { BiMinus } from "react-icons/bi";
+
+
 
 const Question = () => {
+  const [ viewQuestionOne, setViewQuestionOne ] = useState(false);
+
+  const viewQuestOne = () => {
+    setViewQuestionOne(!viewQuestionOne);
+  }
+
   return (
     <>
       <div className="mx-10 md:mx-10 md:my-10 my5">
@@ -18,7 +29,7 @@ const Question = () => {
           <div className="border-[1px] border-slate-400 rounded-lg bg-slate-50 ">
             <h2 className="text-lg md:text-xl font-bold text-black flex justify-between md:px-10 px-10 md:py-5 py-5">
               01. Quelles sont nos offres ?
-              <VscWorkspaceTrusted className=" text-2xl md:text-xl text-green-500" />
+              <FaPlus className=" text-2xl md:text-2xl text-green-500" />
             </h2>
             <div className="hidden md:hidden"></div>
           </div>
@@ -28,7 +39,7 @@ const Question = () => {
               02. Comment devenir partenaire de la MUSDEEL ?
               <VscWorkspaceTrusted className=" text-2xl md:text-xl text-green-500" />
             </h2>
-            <div className="hidden md:hidden">
+            <div className="hidden md:hidden" >
               The main feature of Whitespace UI that it saves you time. If it
               saves you time, it automatically saves you money. You no longer
               have to create a new UI kit from scratch before starting each
@@ -38,10 +49,16 @@ const Question = () => {
           </div>
 
           <div className="border-[1px] border-slate-400 rounded-lg bg-slate-50 ">
-            <h2 className="text-lg md:text-xl font-bold text-black flex justify-between md:px-10 px-10 md:py-5 py-5">
+            <div className="text-lg md:text-xl font-bold text-black flex justify-between md:px-10 px-10 md:py-5 py-5">
               03. Quelle est la politique de remboursement ?
-              <VscWorkspaceTrusted className=" text-2xl md:text-xl text-green-500" />
-            </h2>
+              <button className=" cursor-pointer" onClick={viewQuestOne}>
+                {viewQuestionOne ? (
+                  <BiMinus className=" text-2xl md:text-xl text-green-500" />
+                ) : (
+                  <VscWorkspaceTrusted className=" text-2xl md:text-xl text-green-500" />
+                )}
+              </button>
+            </div>
             <div className=" hidden md:hidden"></div>
           </div>
 
@@ -55,7 +72,8 @@ const Question = () => {
 
           <div className="border-[1px] border-slate-400 rounded-lg bg-slate-50 ">
             <h2 className="text-lg md:text-xl font-bold text-black flex justify-between md:px-10 px-10 md:py-5 py-5">
-              05. Quelles sont les méthodes de paiement viables dans la MUSDEEL ?
+              05. Quelles sont les méthodes de paiement viables dans la MUSDEEL
+              ?
               <VscWorkspaceTrusted className=" text-2xl md:text-xl text-green-500" />
             </h2>
             <div className="hidden md:hidden"></div>
