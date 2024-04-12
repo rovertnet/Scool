c70648c2161154bbf09773d724be1e3798ce3f88
Robert Matundu <matundukabamba@gmail.com>
2024-04-13 01:05:55 +0200

    another commit

--- src/component/cHome/Question.jsx
+++ src/component/cHome/Question.jsx
@@ -31,12 +31,12 @@ const Question = () => {
 
   return (
     <>
-      <div className="mx-10 md:mx-10 md:my-10 my5">
+      <div className="mx-10 md:mx-32 md:my-10 my-5">
         <div className="text-center md:py-10 py-10">
-          <h1 className=" text-xl md:text-2xl text-center font-extrabold uppercase">
+          <h1 className=" text-lg md:text-2xl text-center font-extrabold uppercase">
             Questions fréquemment posées
           </h1>
-          <p className=" md:pt-3 pt-3">
+          <p className=" md:pt-3 pt-3 md:text-base text-sm">
             Nous avons essayé de répondre aux questions les plus courantes.{" "}
             <br /> Si vous en avez d autres, n hésitez pas à contacter notre{" "}
             <br /> équipe amicale.
@@ -44,7 +44,7 @@ const Question = () => {
         </div>
         <div className=" grid grid-cols-1 gap-10">
           <div className="border-[1px] border-slate-300 rounded-lg bg-slate-50 shadow-md">
-            <div className="text-lg md:text-xl font-bold text-black flex justify-between md:px-10 px-10 md:py-5 py-5">
+            <div className="text-lg md:text-xl font-bold text-black flex justify-between gap-10 md:px-10 px-10 md:py-5 py-5">
               01. Quelles sont nos offres ?
               <button
                 className=" cursor-pointer hover:bg-slate-200 hover:p-2 hover:md:p-2 hover:rounded-full"
