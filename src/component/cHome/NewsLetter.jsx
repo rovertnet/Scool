07ea72b05fbc6ac9d78df10d735ff5bfbda97d8c
Robert Matundu <matundukabamba@gmail.com>
2024-04-13 22:02:46 +0200

    another commit

---
+++ src/component/cHome/NewsLetter.jsx
@@ -0,0 +1,16 @@
+
+const NewsLetter = () => {
+  return (
+    <>
+      <div className="md:mx-10 mx-10 md:my-10 my-10">
+        <div className="flex flex-col justify-start items-start bg-green-700 md:px-10 px-10 md:py-5 py-6">
+          <h2 className=" md:text-lg text-lg leading-normal">
+            Entrez votre adresse e mail et recevez de nouvelles chaque matin
+          </h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NewsLetter
