676f13ada01c2c0e9f5f95d2f430f9fd740000a8
Robert Matundu <matundukabamba@gmail.com>
2024-04-13 16:11:36 +0200

    another commit

--- src/component/cHome/Partenaire.jsx
+++ src/component/cHome/Partenaire.jsx
@@ -2,7 +2,11 @@
 const Partenaire = () => {
   return (
     <>
-      <div className="md:mx-10 mx-10 md:my-10 my-10"></div>
+      <div className="md:mx-10 mx-10 md:my-10 my-10">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-10">
+          div
+        </div>
+      </div>
     </>
   )
 }
