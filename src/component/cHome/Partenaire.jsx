5208ca2a79971645a939fac55c53d398f2330475
Robert Matundu <matundukabamba@gmail.com>
2024-04-13 16:43:17 +0200

    another commit

--- src/component/cHome/Partenaire.jsx
+++ src/component/cHome/Partenaire.jsx
@@ -3,7 +3,8 @@ const Partenaire = () => {
   return (
     <>
       <div className="md:mx-10 mx-10 md:my-10 my-10">
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-10 ">
+        <h2 className="text-xl uppercase md:text-2xl font-bold text-center py-7 md:py-10 md:mb-10 mb-8">Nos Partenaires</h2>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-10 md:px-10 px-10 text-center">
           <div className="flex space-x-3">
             <span className=" text-2xl md:text-2xl font-bold text-black">Noviexar</span>
           </div>
