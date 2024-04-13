4fb6fee8ad7ed3c0199234f603393c0ebdefb3c7
Robert Matundu <matundukabamba@gmail.com>
2024-04-13 14:42:28 +0200

    another commit

---
+++ src/component/cHome/ComposantsMere.jsx
@@ -0,0 +1,10 @@
+
+const ComposantsMere = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default ComposantsMere
---
+++ src/component/cHome/Contact.jsx
@@ -0,0 +1,14 @@
+
+const Contact = () => {
+  return (
+    <>
+      <div className="md:mx-10 mx-10 md:my-10 my-9">
+        <div className=" flex flex-col gap-10 md:py-7 py-7 md:px-10 px-10 bg-green-700 rounded-lg text-center">
+          <h2 className="text-center text-xl md:text-2xl font-bold text-slate-100 leading-normal">Vous avez encore de questions ?</h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Contact
