55ee6523ff1e836b6f5c31a94cfb2a3295aabf6e
Robert Matundu <matundukabamba@gmail.com>
2024-04-13 16:32:57 +0200

    another commit

--- src/component/cHome/Partenaire.jsx
+++ src/component/cHome/Partenaire.jsx
@@ -4,11 +4,41 @@ const Partenaire = () => {
     <>
       <div className="md:mx-10 mx-10 md:my-10 my-10">
         <div className="grid grid-cols-2 md:grid-cols-4 gap-10">
-          div
+          <div className="flex space-x-3">
+            <span className=" text-2xl md:text-2xl font-bold text-black">Noviexar</span>
+          </div>
+
+          <div className="flex space-x-3">
+            <span className=" text-2xl md:text-2xl font-bold text-black">Skyewave</span>
+          </div>
+
+          <div className="flex space-x-3">
+            <span className=" text-2xl md:text-2xl font-bold text-black">NexaCore</span>
+          </div>
+
+          <div className="flex space-x-3">
+            <span className=" text-2xl md:text-2xl font-bold text-black">EliteXcel</span>
+          </div>
+
+          <div className="flex space-x-3">
+            <span className=" text-2xl md:text-2xl font-bold text-black">Zenithron</span>
+          </div>
+
+          <div className="flex space-x-3">
+            <span className=" text-2xl md:text-2xl font-bold text-black">Ennovatek</span>
+          </div>
+
+          <div className="flex space-x-3">
+            <span className=" text-2xl md:text-2xl font-bold text-black">Infinitumix</span>
+          </div>
+
+          <div className="flex space-x-3">
+            <span className=" text-2xl md:text-2xl font-bold text-black">SpectraFuse</span>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default Partenaire
