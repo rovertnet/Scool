2db955c72a1c53985f1a7594e65fe8172137f1f7
Robert Matundu <matundukabamba@gmail.com>
2024-04-13 16:54:16 +0200

    another commit

--- src/component/cHome/Partenaire.jsx
+++ src/component/cHome/Partenaire.jsx
@@ -1,45 +1,72 @@
+import { MdOutlineWatchLater } from "react-icons/gi";
 
 const Partenaire = () => {
   return (
     <>
       <div className="md:mx-10 mx-10 md:my-10 my-10">
-        <h2 className="text-xl uppercase md:text-2xl font-bold text-center py-7 md:py-10 md:mb-10 mb-8">Nos Partenaires</h2>
+        <h2 className="text-xl uppercase md:text-2xl font-bold text-center py-7 md:py-10 md:mb-10 mb-8">
+          Nos Partenaires
+        </h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-10 md:px-10 px-10 text-center">
           <div className="flex space-x-3">
-            <span className=" text-2xl md:text-2xl font-bold text-black">Noviexar</span>
+            <MdOutlineWatchLater className=" font-extrabold text-4xl text-green-600" />
+            <span className=" text-2xl md:text-2xl font-bold text-black">
+              Noviexar
+            </span>
           </div>
 
           <div className="flex space-x-3">
-            <span className=" text-2xl md:text-2xl font-bold text-black">Skyewave</span>
+            <MdOutlineWatchLater className=" font-extrabold text-4xl text-green-600" />
+            <span className=" text-2xl md:text-2xl font-bold text-black">
+              Skyewave
+            </span>
           </div>
 
           <div className="flex space-x-3">
-            <span className=" text-2xl md:text-2xl font-bold text-black">NexaCore</span>
+            <MdOutlineWatchLater className=" font-extrabold text-4xl text-green-600" />
+            <span className=" text-2xl md:text-2xl font-bold text-black">
+              NexaCore
+            </span>
           </div>
 
           <div className="flex space-x-3">
-            <span className=" text-2xl md:text-2xl font-bold text-black">EliteXcel</span>
+            <MdOutlineWatchLater className=" font-extrabold text-4xl text-green-600" />
+            <span className=" text-2xl md:text-2xl font-bold text-black">
+              EliteXcel
+            </span>
           </div>
 
           <div className="flex space-x-3">
-            <span className=" text-2xl md:text-2xl font-bold text-black">Zenithron</span>
+            <MdOutlineWatchLater className=" font-extrabold text-4xl text-green-600" />
+            <span className=" text-2xl md:text-2xl font-bold text-black">
+              Zenithron
+            </span>
           </div>
 
           <div className="flex space-x-3">
-            <span className=" text-2xl md:text-2xl font-bold text-black">Ennovatek</span>
+            <MdOutlineWatchLater className=" font-extrabold text-4xl text-green-600" />
+            <span className=" text-2xl md:text-2xl font-bold text-black">
+              Ennovatek
+            </span>
           </div>
 
           <div className="flex space-x-3">
-            <span className=" text-2xl md:text-2xl font-bold text-black">Infinitumix</span>
+            <MdOutlineWatchLater className=" font-extrabold text-4xl text-green-600" />
+            <span className=" text-2xl md:text-2xl font-bold text-black">
+              Infinitumix
+            </span>
           </div>
 
           <div className="flex space-x-3">
-            <span className=" text-2xl md:text-2xl font-bold text-black">SpectraFuse</span>
+            <MdOutlineWatchLater className=" font-extrabold text-4xl text-green-600" />
+            <span className=" text-2xl md:text-2xl font-bold text-black">
+              SpectraFuse
+            </span>
           </div>
         </div>
       </div>
     </>
   );
-}
+};
 
-export default Partenaire
+export default Partenaire;
