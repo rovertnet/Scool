4736f06191eb90c2d6fb8bfd4efb30c6971c6698
Robert Matundu <matundukabamba@gmail.com>
2024-04-13 16:00:08 +0200

    another commit

--- src/component/cHome/Contact.jsx
+++ src/component/cHome/Contact.jsx
@@ -13,8 +13,8 @@ const Contact = () => {
             que vous recherchiez. Veuillez nous contacter et nous serons heureux
             de vous aider.
           </p>
-          <button className=" bg-slate-900 text-lg md:text-xl font-semibold hover:bg-slate-800 text-slate-50 flex gap-4 rounded-lg px-7 md:px-7 py-4 md:py-3">
-            <LiaPhoneVolumeSolid className=" font-bold text-lg md:text-4xl text-slate-50" />
+          <button className=" bg-slate-900 text-base md:text-xl font-semibold hover:bg-slate-800 text-slate-50 flex gap-4 rounded-lg px-10 md:px-7 py-4 md:py-2">
+            <LiaPhoneVolumeSolid className=" font-bold text-4xl md:text-4xl text-slate-50" />
             Contactez-nous
           </button>
         </div>
