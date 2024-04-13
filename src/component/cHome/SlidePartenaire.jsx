4bc9422f406393bdbbc156ba54d3d0c8f2cf9d5c
Robert Matundu <matundukabamba@gmail.com>
2024-04-13 18:00:52 +0200

    another commit

--- src/component/cHome/SlidePartenaire.jsx
+++ src/component/cHome/SlidePartenaire.jsx
@@ -5,24 +5,29 @@ import "swiper/css";
 const SlidePartenaire = () => {
   return (
     <>
-      <div className="flex justify-center items-center h-screen">
-        <div className=" md:max-w-5xl max-w-3xl">
-          <Swiper
-            spaceBetween={50}
-            slidesPerView={3}
-            onSlideChange={() => console.log("slide change")}
-            onSwiper={(swiper) => console.log(swiper)}
-          >
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            ...
-          </Swiper>
+      <div className="md:mx-10 mx-10 md:my-10 my-10">
+        <h2 className="text-xl uppercase md:text-2xl font-bold text-center py-7 md:py-10 md:mb-10 mb-8">
+          Nos Partenaires
+        </h2>
+        <div className="flex justify-center items-center h-screen text-black font-extrabold">
+          <div className=" md:max-w-5xl max-w-3xl">
+            <Swiper
+              spaceBetween={50}
+              slidesPerView={3}
+              onSlideChange={() => console.log("slide change")}
+              onSwiper={(swiper) => console.log(swiper)}
+            >
+              <SwiperSlide>Noviexar</SwiperSlide>
+              <SwiperSlide>Skyewave</SwiperSlide>
+              <SwiperSlide>NexaCore</SwiperSlide>
+              <SwiperSlide>EliteXcel</SwiperSlide>
+              <SwiperSlide>Zenithron</SwiperSlide>
+              <SwiperSlide>Ennovatek</SwiperSlide>
+              <SwiperSlide>Infinitumix</SwiperSlide>
+              <SwiperSlide>SpectraFuse</SwiperSlide>
+              ...
+            </Swiper>
+          </div>
         </div>
       </div>
     </>
