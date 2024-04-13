2880e5fd78a972cfc4159421db78a59f59334f2e
Robert Matundu <matundukabamba@gmail.com>
2024-04-13 18:47:19 +0200

    another commit

--- src/component/cHome/SlidePartenaire.jsx
+++ src/component/cHome/SlidePartenaire.jsx
@@ -1,6 +1,10 @@
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const SlidePartenaire = () => {
   return (
@@ -9,11 +13,15 @@ const SlidePartenaire = () => {
         <h2 className="text-xl uppercase md:text-2xl font-bold text-center py-7 md:py-10 md:mb-10 mb-8">
           Nos Partenaires
         </h2>
-        <div className="flex justify-center items-center h-screen text-black font-extrabold">
+        <div className="flex justify-center items-center text-black font-extrabold">
           <div className=" md:max-w-5xl max-w-3xl">
             <Swiper
-              spaceBetween={50}
+              modules={[Navigation, Pagination, Scrollbar, A11y]}
+              spaceBetween={100}
               slidesPerView={3}
+              navigation
+              pagination={{ clickable: true }}
+              scrollbar={{ draggable: false }}
               onSlideChange={() => console.log("slide change")}
               onSwiper={(swiper) => console.log(swiper)}
             >
@@ -25,7 +33,6 @@ const SlidePartenaire = () => {
               <SwiperSlide>Ennovatek</SwiperSlide>
               <SwiperSlide>Infinitumix</SwiperSlide>
               <SwiperSlide>SpectraFuse</SwiperSlide>
-              ...
             </Swiper>
           </div>
         </div>
