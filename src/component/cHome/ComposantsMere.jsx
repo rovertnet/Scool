aafefd37a0adcda82ef24cc07d7d7546e40200dc
Robert Matundu <matundukabamba@gmail.com>
2024-04-13 22:18:31 +0200

    another commit

--- src/component/cHome/ComposantsMere.jsx
+++ src/component/cHome/ComposantsMere.jsx
@@ -5,6 +5,7 @@ import Contact from "./Contact";
 import Person from "./Person";
 import Question from "./Question";
 import Partenaire from "./Partenaire";
+import NewsLetter from "./NewsLetter";
 
 const ComposantsMere = () => {
   return (
@@ -15,6 +16,7 @@ const ComposantsMere = () => {
       <Question />
       <Contact />
       <Partenaire />
+      <NewsLetter />
 
       <MdOutlet />
     </>
