4a44868218cdc1aabb8f3c55573e4ddcc1ddfe33
Robert Matundu <matundukabamba@gmail.com>
2024-04-13 22:03:44 +0200

    another commit

--- src/component/cHome/NewsLetter.jsx
+++ src/component/cHome/NewsLetter.jsx
@@ -4,7 +4,7 @@ const NewsLetter = () => {
     <>
       <div className="md:mx-10 mx-10 md:my-10 my-10">
         <div className="flex flex-col justify-start items-start bg-green-700 md:px-10 px-10 md:py-5 py-6">
-          <h2 className=" md:text-lg text-lg leading-normal">
+          <h2 className=" md:text-lg text-lg leading-normal text-slate-50">
             Entrez votre adresse e mail et recevez de nouvelles chaque matin
           </h2>
         </div>
