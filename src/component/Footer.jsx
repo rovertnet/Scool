c536709910125460ccc3e977e4a376503915ed5a
Robert Matundu <matundukabamba@gmail.com>
2024-04-15 12:12:28 +0200

    another commit

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,3 +1,8 @@
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { BsFacebook } from "react-icons/bs";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
+
 
 const Footer = () => {
   return (
@@ -10,8 +15,16 @@ const Footer = () => {
           <div className="grid grid-cols-2 md:grid-cols-4 gap-10">
             <div className="flex flex-col gap-5">
               <p className="text-slate-100 font-medium md:text-base text-lg py-4 md:py-5">
-                
+                Nous sommes un système qui permet à nos adhérents, contre
+                paiement d’une cotisation, de se faire rembourser des frais de
+                santé qui ne sont pas pris en charge par la Sécurité sociale
               </p>
+              <div className="flex space-x-3 md:space-x-3">
+                <FaSquareXTwitter className="font-bold text-xl md:text-xl" />
+                <BsFacebook className="font-bold text-xl md:text-xl" />
+                <FaSquareInstagram className="font-bold text-xl md:text-xl" />
+                <FaLinkedin className="font-bold text-xl md:text-xl" />
+              </div>
             </div>
             <div className="flex flex-col gap-5"></div>
             <div className="flex flex-col gap-5"></div>
@@ -21,6 +34,6 @@ const Footer = () => {
       </div>
     </>
   );
-}
+};
 
-export default Footer
+export default Footer;
