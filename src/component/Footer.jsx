dd0d5210f391baa143150e9ed20b8c00cc5e4ecc
Robert Matundu <matundukabamba@gmail.com>
2024-04-15 11:24:19 +0200

    another commit

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,10 +3,13 @@ const Footer = () => {
   return (
     <>
       <div className="min-h-full">
-        <div className="bg-slate-900 md:px-10 px-10 md:py-8 py-8"></div>
+        <div className="bg-slate-900 md:px-10 px-10 md:py-8 py-8">
+          <h1 className="text-start font-bold text-xl md:text-2xl text-white leading-normal">LA MUSDEEL</h1>
+          div.
+        </div>
       </div>
     </>
-  )
+  );
 }
 
 export default Footer
