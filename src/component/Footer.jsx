5cae2e6b70f9374d6a585120e9fc0ac981b65d8b
Robert Matundu <matundukabamba@gmail.com>
2024-04-15 11:15:57 +0200

    another commit

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <>
       <div className="min-h-full">
-        <div className="bg-slate-900 md:px-10 px-10"></div>
+        <div className="bg-slate-900 md:px-10 px-10 md:py-8 py-8"></div>
       </div>
     </>
   )
