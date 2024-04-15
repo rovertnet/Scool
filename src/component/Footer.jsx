b3582f0d62042741f0f603a39ccc27bae27a1a51
Robert Matundu <matundukabamba@gmail.com>
2024-04-15 11:54:48 +0200

    another commit

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -5,7 +5,9 @@ const Footer = () => {
       <div className="min-h-full">
         <div className="bg-slate-900 md:px-10 px-10 md:py-8 py-8">
           <h1 className="text-start font-bold text-xl md:text-2xl text-white leading-normal">LA MUSDEEL</h1>
-          div.
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-10">
+            <div className="flex flex-col gap-5"></div>
+          </div>
         </div>
       </div>
     </>
