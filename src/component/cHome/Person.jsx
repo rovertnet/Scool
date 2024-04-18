7522cd8e479d6b2f5ff9017abab4e2f45ec6e5fd
Robert Matundu <matundukabamba@gmail.com>
2024-04-18 23:53:20 +0200

    another commit

--- src/component/cHome/Person.jsx
+++ src/component/cHome/Person.jsx
@@ -14,9 +14,9 @@ const Person = () => {
         </div>
         <div className=" grid md:grid-cols-3 grid-cols-1 gap-5 md:mt-5 mt-10 ">
           <div className="flex md:flex-col flex-row md:px-12 px-10 mx-10 md:mx-0">
-            <div className="bg-slate-100 px-12 md:px-5 text-center md:py-9 py-9">
-              <img src="../../public/p1.png" alt="" className=" w-10 h-10" />
-              <div>Traditionnelle</div>
+            <div className="bg-slate-100 px-12 md:px-3 text-center items-center flex flex-col justify-center gap-3 md:py-9 py-9">
+              <img src="../../public/p1.png" alt="" className=" w-10 h-10 items-center" />
+              <div>Traditionnelle</div> 
             </div>
             <div className="bg-green-600 text-center md:py-9 py-9 px-4 md:px-5 text-slate-100">
               <div>Sérieuse</div>
