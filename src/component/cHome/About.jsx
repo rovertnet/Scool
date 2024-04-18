9a51c047ec8e2a122d23326624a13028cef52d22
Robert Matundu <matundukabamba@gmail.com>
2024-04-18 21:45:41 +0200

    another commit

--- src/component/cHome/About.jsx
+++ src/component/cHome/About.jsx
@@ -4,12 +4,12 @@ import { MdOutlineSettingsInputComposite } from "react-icons/md";
 const About = () => {
   return (
     <>
-      <div className=" mt-20 md:mt-20 md:mb-14 mb-12 px-10 md:px-12 lg:my-20 lg:px-20 block">
+      <div className=" mt-20 md:mt-20 md:mb-14 mb-12 px-10 md:px-12 md:py-20 py-10 lg:my-20 lg:px-10 block">
         <div className="flex flex-col md:flex-row justify-center items-center md:space-x-10">
           <>
             <img
               src="../../public/image/img1.png"
-              className="w-full md:w-1/1 h-full rounded-lg"
+              className="w-full md:w-1/1 h-[70%] rounded-lg"
               alt="about image"
             />
           </>
