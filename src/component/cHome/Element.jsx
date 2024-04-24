3a4a5048b4465a8f8a883aa846250185cbf96ddb
Robert Matundu <matundukabamba@gmail.com>
2024-04-24 21:04:48 +0200

    last commit

--- src/component/cHome/Element.jsx
+++ src/component/cHome/Element.jsx
@@ -2,9 +2,18 @@
 const Element = () => {
   return (
     <>
-      
+      <div className="my-10 md:my-14 px-10 md:px-12 block md:w-full">
+        <div className="text-center md:pb-10 pb-10">
+          <h2 className=" text-2xl text-slate-900 font-bold md:text-2xl">
+            4 ELEMENTS NOUS DEFINISSENT
+          </h2>
+          <h3 className=" text-black text-lg uppercase font-normal">
+            Our services are designed to cater to your specific needs and goals
+          </h3>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Element
