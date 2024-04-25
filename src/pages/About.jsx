ee1748701929e7d8a888aa705433366dcee9e92f
Robert Matundu <matundukabamba@gmail.com>
2024-04-25 19:34:37 +0200

    last commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom"
 import Menu from "../component/cHeader/Menu";
 import Tout from "../component/cAbout/Tout";
+import Footer from "../component/Footer";
 
 
 const About = () => {
