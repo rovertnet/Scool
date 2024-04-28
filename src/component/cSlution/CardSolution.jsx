6023232b0a4ad5b4a0d0e1b96f130ae40c4d8a12
Robert Matundu <matundukabamba@gmail.com>
2024-04-28 19:31:12 +0200

    last commit

--- src/component/cSlution/CardSolution.jsx
+++ src/component/cSlution/CardSolution.jsx
@@ -4,7 +4,23 @@ const CardSolution = () => {
   return (
     <>
       <div className="mt-20 md:mt-20 md:mb-14 mb-12 px-10 md:px-12 md:py-20 py-10 block">
-        <div className=""></div>
+        <div className=" md:mx-8 mx-6 grid grid-cols-1 md:grid-cols-3 gap-5">
+          <div className=" bg-slate-300 dark:bg-slate-800 rounded-lg overflow-hidden">
+            <img
+              src=""
+              alt=""
+              className=" w-full h-36 md:h-48 object-cover"
+            />
+            <div className=" text-gray-600 dark:text-gray-300 p-5 w-full">
+              <h3 className="text-xl md:text-xl mb-3 md:mb-4 font-semibold">
+                
+              </h3>
+              <p className=" flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
+                
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
