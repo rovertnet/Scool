d08b3571ce989b5693ee4308f98b6f8b531ab9c7
Robert Matundu <matundukabamba@gmail.com>
2024-04-28 19:24:19 +0200

    last commit

--- src/component/cSlution/CardSolution.jsx
+++ src/component/cSlution/CardSolution.jsx
@@ -3,9 +3,11 @@
 const CardSolution = () => {
   return (
     <>
-      <div className=""></div>
+      <div className="mt-20 md:mt-20 md:mb-14 mb-12 px-10 md:px-12 md:py-20 py-10 block">
+        <div className=""></div>
+      </div>
     </>
-  )
+  );
 }
 
 export default CardSolution
