d060e50ff9f811266672984d6423f11f3ef1f84a
Robert Matundu <matundukabamba@gmail.com>
2024-04-28 19:38:10 +0200

    last commit

--- src/component/cSlution/CardSolution.jsx
+++ src/component/cSlution/CardSolution.jsx
@@ -4,20 +4,61 @@ const CardSolution = () => {
   return (
     <>
       <div className="mt-20 md:mt-20 md:mb-14 mb-12 px-10 md:px-12 md:py-20 py-10 block">
-        <div className=" md:mx-8 mx-6 grid grid-cols-1 md:grid-cols-3 gap-5">
+        <div className=" md:mx-8 mx-6 grid grid-cols-2 md:grid-cols-3 gap-5">
           <div className=" bg-slate-300 dark:bg-slate-800 rounded-lg overflow-hidden">
             <img
-              src=""
+              src="../../public/11.jpg"
               alt=""
               className=" w-full h-36 md:h-48 object-cover"
             />
-            <div className=" text-gray-600 dark:text-gray-300 p-5 w-full">
-              <h3 className="text-xl md:text-xl mb-3 md:mb-4 font-semibold">
-                
+            <div className=" text- p-5 w-full">
+              <h3 className="text-2xl md:text-2xl mb-3 md:mb-4 font-bold">
+                Prestation 1
               </h3>
-              <p className=" flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
-                
+              <p className=" flex flex-wrap gap-2 flex-row items-center justify-start text-lg md:text-lg">
+                Lorem ipsum dolor sit amet, consectetur adipiscing Quisque
+                placerat Convallis felis vitae tortor augue. Velit nascetur
+                massa in.
               </p>
+              <p>Learn more </p>
+            </div>
+          </div>
+
+          <div className=" bg-slate-300 dark:bg-slate-800 rounded-lg overflow-hidden">
+            <img
+              src="../../public/11.jpg"
+              alt=""
+              className=" w-full h-36 md:h-48 object-cover"
+            />
+            <div className=" text- p-5 w-full">
+              <h3 className="text-2xl md:text-2xl mb-3 md:mb-4 font-bold">
+                Prestation 1
+              </h3>
+              <p className=" flex flex-wrap gap-2 flex-row items-center justify-start text-lg md:text-lg">
+                Lorem ipsum dolor sit amet, consectetur adipiscing Quisque
+                placerat Convallis felis vitae tortor augue. Velit nascetur
+                massa in.
+              </p>
+              <p>Learn more </p>
+            </div>
+          </div>
+          
+          <div className=" bg-slate-300 dark:bg-slate-800 rounded-lg overflow-hidden">
+            <img
+              src="../../public/11.jpg"
+              alt=""
+              className=" w-full h-36 md:h-48 object-cover"
+            />
+            <div className=" text- p-5 w-full">
+              <h3 className="text-2xl md:text-2xl mb-3 md:mb-4 font-bold">
+                Prestation 1
+              </h3>
+              <p className=" flex flex-wrap gap-2 flex-row items-center justify-start text-lg md:text-lg">
+                Lorem ipsum dolor sit amet, consectetur adipiscing Quisque
+                placerat Convallis felis vitae tortor augue. Velit nascetur
+                massa in.
+              </p>
+              <p>Learn more </p>
             </div>
           </div>
         </div>
