8cf3af8f8fbbbdb7e90656b84775f9e0fa9b1371
Robert Matundu <matundukabamba@gmail.com>
2024-04-30 15:23:29 +0200

    last commit

--- src/component/cSlution/SuperComp.jsx
+++ src/component/cSlution/SuperComp.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import SolutionBanner from "./SolutioBanner"
 import CardSolution from "./CardSolution";
+import ContactSolution from "./ContactSolution"
 
 const SuperComp = () => {
   return (
     <>
       <SolutionBanner />
       <CardSolution />
+      <ContactSolution />
       <Outlet />
     </>
   );
