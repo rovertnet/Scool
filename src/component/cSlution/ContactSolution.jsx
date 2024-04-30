f3aec986e2272d67482e9e5bd15ee8a340847551
Robert Matundu <matundukabamba@gmail.com>
2024-04-30 14:54:38 +0200

    last commit

--- src/component/cSlution/ContactSolution.jsx
+++ src/component/cSlution/ContactSolution.jsx
@@ -5,7 +5,7 @@ const ContactSolution = () => {
     <>
       <div className="mt-10 md:mt-10 md:mb-14 mb-12 px-10 md:px-12 md:py-10 py-10 block">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-10 md:gap-10">
-          
+          <div className="flex flex-col md:"></div>
         </div>
       </div>
     </>
