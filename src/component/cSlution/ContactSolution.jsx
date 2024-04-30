c2986e94364e59dc086c62aea61f17e6a8a9836a
Robert Matundu <matundukabamba@gmail.com>
2024-04-30 14:53:33 +0200

    last commit

---
+++ src/component/cSlution/ContactSolution.jsx
@@ -0,0 +1,15 @@
+
+
+const ContactSolution = () => {
+  return (
+    <>
+      <div className="mt-10 md:mt-10 md:mb-14 mb-12 px-10 md:px-12 md:py-10 py-10 block">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-10 md:gap-10">
+          
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ContactSolution
