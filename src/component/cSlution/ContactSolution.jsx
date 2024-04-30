344f96fddd95ae31077030e7d323ef442fe5d221
Robert Matundu <matundukabamba@gmail.com>
2024-04-30 15:28:46 +0200

    last commit

--- src/component/cSlution/ContactSolution.jsx
+++ src/component/cSlution/ContactSolution.jsx
@@ -7,6 +7,10 @@ const ContactSolution = () => {
   return (
     <>
       <div className="mt-10 md:mt-10 md:mb-14 mb-12 px-10 md:px-12 md:py-10 py-10 block">
+        <div className="flex">
+          <h3 className=" font-medium text-lg md:text-lg text-green-700">Entrer  en  contact</h3>
+          <h2 className=" font-extrabold">Contact</h2>
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-4 gap-10 md:gap-10">
           {/* Nos card */}
           <div className="flex flex-col md:flex-col justify-start items-start gap-8 md:gap-8 bg-green-800 p-5 md:p-7">
