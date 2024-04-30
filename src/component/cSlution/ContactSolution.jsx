8855dc09b84a516f07c77a9d4759aca340b1d200
Robert Matundu <matundukabamba@gmail.com>
2024-04-30 15:48:03 +0200

    last commit

--- src/component/cSlution/ContactSolution.jsx
+++ src/component/cSlution/ContactSolution.jsx
@@ -7,14 +7,18 @@ const ContactSolution = () => {
   return (
     <>
       <div className="mt-10 md:mt-10 md:mb-14 mb-12 px-10 md:px-12 md:py-10 py-10 block">
-        <div className="flex md:flex-col flex-col gap-3 md:gap-3 md:py-10 py-10">
-          <h3 className=" font-medium text-lg md:text-lg text-green-700">Entrer  en  contact</h3>
-          <h2 className=" font-extrabold text-2xl md:text-2xl text-black uppercase">Contact</h2>
+        <div className="flex md:flex-col flex-col gap-2 md:gap-2 md:py-10 py-10 justify-center items-center">
+          <h3 className=" font-medium text-lg md:text-lg text-green-700">
+            Entrer en contact
+          </h3>
+          <h2 className=" font-extrabold text-2xl md:text-2xl text-black uppercase">
+            Contact
+          </h2>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-4 gap-10 md:gap-10">
           {/* Nos card */}
-          <div className="flex flex-col md:flex-col justify-start items-start gap-8 md:gap-8 bg-green-800 p-5 md:p-7">
-            <LiaPhoneVolumeSolid className=" font-bold text-2xl md:text-2xl text-white" />
+          <div className="flex flex-col md:flex-col justify-start items-start gap-3 md:gap-3 bg-green-700 p-5 md:p-7">
+            <LiaPhoneVolumeSolid className=" font-bold text-2xl md:text-4xl text-white" />
             <h2 className=" uppercase font-bold text-2xl md:text-2xl text-white">
               APPEL d’URGENCE
             </h2>
@@ -23,28 +27,28 @@ const ContactSolution = () => {
             </p>
           </div>
 
-          <div className="flex flex-col md:flex-col justify-start items-start gap-8 md:gap-8 bg-black p-5 md:p-7">
-            <IoLocationOutline className=" font-bold text-2xl md:text-2xl text-white" />
+          <div className="flex flex-col md:flex-col justify-start items-start gap-3 md:gap-3 bg-black p-5 md:p-7">
+            <IoLocationOutline className=" font-bold text-2xl md:text-4xl text-white" />
+            <h2 className=" uppercase font-bold text-2xl md:text-2xl text-white">
+              NOS BUREAUX
+            </h2>
+            <p className=" font-semibold text-slate-50 text-lg md:text-lg">
+              0123 Some place <br /> 9876 Some country
+            </p>
+          </div>
+
+          <div className="flex flex-col md:flex-col justify-start items-start gap-3 md:gap-3 bg-red-600 p-5 md:p-7">
+            <BiSolidEnvelopeOpen className=" font-bold text-2xl md:text-4xl text-white" />
             <h2 className=" uppercase font-bold text-2xl md:text-2xl text-white">
               APPEL d’URGENCE
             </h2>
             <p className=" font-semibold text-slate-50 text-lg md:text-lg">
-              (237) 681-812-255 <br /> (237) 666-331-894
+              ildineeesoe@gmil.com <br /> myebstudios@gmail.com
             </p>
           </div>
 
-          <div className="flex flex-col md:flex-col justify-start items-start gap-8 md:gap-8 bg-red-600 p-5 md:p-7">
-            <BiSolidEnvelopeOpen className=" font-bold text-2xl md:text-2xl text-white" />
-            <h2 className=" uppercase font-bold text-2xl md:text-2xl text-white">
-              APPEL d’URGENCE
-            </h2>
-            <p className=" font-semibold text-slate-50 text-lg md:text-lg">
-              (237) 681-812-255 <br /> (237) 666-331-894
-            </p>
-          </div>
-
-          <div className="flex flex-col md:flex-col justify-start items-start gap-8 md:gap-8 bg-green-800 p-5 md:p-7">
-            <MdOutlineWatchLater className=" font-bold text-2xl md:text-2xl text-white" />
+          <div className="flex flex-col md:flex-col justify-start items-start gap-3 md:gap-3 bg-green-700 p-5 md:p-7">
+            <MdOutlineWatchLater className=" font-bold text-2xl md:text-4xl text-white" />
             <h2 className=" uppercase font-bold text-2xl md:text-2xl text-white">
               APPEL d’URGENCE
             </h2>
